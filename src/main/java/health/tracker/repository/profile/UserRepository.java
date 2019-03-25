afff88ce9c89f5aab7149087e5023cb48535f2c9
Mateusz Pomykalski <pomykalskimateusz@gmail.com>
2019-03-25 20:32:19 +0100

    remove findAll method from userRepository

--- src/main/java/health/tracker/repository/profile/UserRepository.java
+++ src/main/java/health/tracker/repository/profile/UserRepository.java
@@ -3,11 +3,9 @@ package health.tracker.repository.profile;
 import health.tracker.repository.DatabaseConnector;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
-public class UserRepository //implements Repository<User>
+public class UserRepository
 {
     public static final String TABLE_NAME = "USERS";
 
@@ -18,7 +16,6 @@ public class UserRepository //implements Repository<User>
         databaseConnector = new DatabaseConnector();
     }
 
- //   @Override
     public boolean save(User user)
     {
         Connection connection = null;
@@ -50,53 +47,6 @@ public class UserRepository //implements Repository<User>
         }
     }
 
-  //  @Override
-    public List<User> findAll()
-    {
-        Connection connection = null;
-        Statement statement = null;
-        ArrayList<User> users = new ArrayList<>();
-
-        try
-        {
-            connection = databaseConnector.prepareConnection();
-
-            statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery(selectQuery());
-
-            while(resultSet.next())
-            {
-                users.add(User
-                        .builder()
-                        .id(resultSet.getLong("id"))
-                        .name(resultSet.getString("name"))
-                        .age(resultSet.getDouble("age"))
-                        .height(resultSet.getDouble("height"))
-                        .weight(resultSet.getDouble("weight"))
-                        .build());
-            }
-
-            try
-            {
-                resultSet.close();
-            }
-            catch (SQLException ignored) {}
-
-            return users;
-        }
-        catch (Exception exception)
-        {
-            System.out.println("Error something wrong during SQL connection or query execution. Here is some details : \n" + exception.getMessage());
-            return Collections.emptyList();
-        }
-        finally
-        {
-            databaseConnector.closeConnection(connection, Collections.singletonList(statement));
-        }
-    }
-
- //   @Override
     public User findById(Long id)
     {
         Connection connection = null;
@@ -171,11 +121,6 @@ public class UserRepository //implements Repository<User>
     private String insertQuery()
     {
         return "INSERT INTO " + TABLE_NAME + "(NAME, AGE, HEIGHT, WEIGHT) VALUES (?,?,?,?);";
-    }
-
-    private String selectQuery()
-    {
-        return "SELECT * FROM " + TABLE_NAME + ";";
     }
 
     private String getByIdQuery(Long id)
