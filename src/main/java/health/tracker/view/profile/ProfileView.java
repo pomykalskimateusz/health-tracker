6db3212f13cf01e833eae9a7e42519be91fbdc6b
Mateusz Pomykalski <pomykalskimateusz@gmail.com>
2019-03-25 20:32:26 +0100

    add lorem ipsum

--- src/main/java/health/tracker/view/profile/ProfileView.java
+++ src/main/java/health/tracker/view/profile/ProfileView.java
@@ -4,7 +4,9 @@ import com.github.appreciated.app.layout.annotations.Caption;
 import com.github.appreciated.app.layout.annotations.Icon;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -33,12 +35,16 @@ public class ProfileView extends VerticalLayout
     public ProfileView()
     {
         User user = userRepository.findById(1L);
-        System.out.println("ZNALAZLO ? " + user.toString());
         profileDetailDialog = new ProfileDetailDialog(user,reloadData());
+
         setupComponents(user);
 
-        add(new H1("User settings"));
-        add(name, age, height, weight, editButton);
+//        add(new H1("User settings"));
+
+        HorizontalLayout mainLayout = new HorizontalLayout();
+        mainLayout.add(formLayout(), plainTextLayout());
+
+        add(mainLayout);
     }
 
     private void setupComponents(User user)
@@ -71,4 +77,25 @@ public class ProfileView extends VerticalLayout
             }
         };
     }
+
+    private VerticalLayout formLayout()
+    {
+        VerticalLayout verticalLayout = new VerticalLayout();
+
+        verticalLayout.add(new H1("User settings"), name, age, height, weight, editButton);
+
+        return verticalLayout;
+    }
+
+    private VerticalLayout plainTextLayout()
+    {
+        VerticalLayout verticalLayout = new VerticalLayout();
+
+        verticalLayout.add(new H1("Some theory"));
+        verticalLayout.add(new Label(THEORY_PLAIN_TEXT));
+
+        return verticalLayout;
+    }
+
+    private final String THEORY_PLAIN_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper commodo feugiat. Proin in elit suscipit, faucibus mi eget, vestibulum felis. Donec placerat faucibus nunc id interdum. Quisque vel fringilla turpis. Etiam a odio a libero lobortis dignissim a sit amet enim. Sed vel lorem ante. Donec tincidunt, massa ut convallis congue, ante arcu tincidunt risus, in fringilla ex augue tempor urna. Morbi sodales eros quis augue sollicitudin, in ultricies nulla rhoncus. Nulla in tincidunt mi, id tempus eros. Sed at lorem urna. In a aliquet ante, sed scelerisque arcu. Nulla tempor vehicula nibh eu placerat. Phasellus feugiat congue est, quis suscipit odio finibus sit amet. Duis ac neque eget est pulvinar viverra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.";
 }
